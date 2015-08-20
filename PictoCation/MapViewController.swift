a8fda5d52295dcc39ca9833677d39f30a17f68ed
Akshay Bharath <akshay@animoto.com>
2015-08-20 12:50:32 -0400

    Revert "Added new UberVC and model"
    This reverts commit aefa2c05ee2e68994b80327d634135f49eb312c0.

--- PictoCation/MapViewController.swift
+++ PictoCation/MapViewController.swift
@@ -182,7 +182,7 @@ class MapViewController: UIViewController {
       selectedHastagTopic = place.stringByReplacingOccurrencesOfString(" ", withString: "")
       navigationController!.view.addSubview(bgOverlay)
       navigationController!.view.addSubview(optionsView)
-      UIView.animateWithDuration(0.4) {
+      UIView.animateWithDuration(0.2) {
         self.bgOverlay.alpha = 0.9
         var optionsFrame = self.optionsView.frame
         optionsFrame.origin.y = (self.bgOverlay.frame.size.height / 2) - (optionsFrame.size.height / 2)
@@ -385,17 +385,6 @@ extension MapViewController: OptionsDelegate {
   
   // TODO
   func uber() {
-    UIView.animateWithDuration(0.2, animations: {
-      var optionsFrame = self.optionsView.frame
-      optionsFrame.origin.y = self.view.frame.maxY
-      self.optionsView.frame = optionsFrame
-      self.bgOverlay.alpha = 0.0
-      }) {
-        (value: Bool) in
-        self.bgOverlay.removeFromSuperview()
-        self.optionsView.removeFromSuperview()
-        self.performSegueWithIdentifier("show uber", sender: self)
-    }
-
+    cancel()
   }
 }
